f9af0d2c366abe2e84287912298351363bd15c00
Will brown <wtbrown10@gmail.com>
2023-01-15 19:21:39 -0500

    first commit

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,11 @@
+
+<html>
+<head>
+    <title> My Login Page - JSP</title>
+</head>
+<body>
+Welcome to the login page ${name}! - JSP
+
+
+</body>
+</html>
---
+++ src/test/java/com/in28minutes/springboot/mywebapp/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.mywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

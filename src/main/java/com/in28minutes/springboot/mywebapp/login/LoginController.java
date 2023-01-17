2622054318c9d6e42ede22629b8d46606ff05482
Will brown <wtbrown10@gmail.com>
2023-01-16 21:55:41 -0500

    logger

--- src/main/java/com/in28minutes/springboot/mywebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/mywebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.mywebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-
+    //a more professional way to log info vs system.out.println
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Request param is " + name);
+        logger.info("Request param is {}", name); // can either put info level, debug, warn, error etc...
+        //System.out.println("Request param is " + name);
         return "login";
     }
 }
